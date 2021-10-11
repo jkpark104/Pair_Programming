d2c8d9e62ca36099c268b9536361f0e90449dd25
jkpark104 <jkrang104@gmail.com>
2021-10-11 14:34:21 +0900

    feat: Implement navigation toggle-menu
    1. Used localStroage, Implement nav toggle-menu

--- 1.Toggle_side_nav/main.js
+++ 1.Toggle_side_nav/main.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+const $nav = document.querySelector('.container nav');
+const $toggle = document.querySelector('.toggle');
+
+window.addEventListener('DOMContentLoaded', () => {
+  localStorage.getItem('toggleStatus') === 'true'
+    ? $nav.classList.add('active')
+    : $nav.classList.remove('active');
+});
+
+$toggle.onclick = () => {
+  if ($nav.classList.contains('active')) {
+    localStorage.clear();
+    localStorage.setItem('toggleStatus', false);
+    $nav.classList.remove('active');
+  } else {
+    localStorage.clear();
+    localStorage.setItem('toggleStatus', true);
+    $nav.classList.add('active');
+  }
+};
