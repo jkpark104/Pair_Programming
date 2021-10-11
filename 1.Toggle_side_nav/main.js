b90953f2cc2273c1127b13f716419baff82f5007
jkpark104 <jkrang104@gmail.com>
2021-10-11 15:57:01 +0900

    resolve: Refactor envent handling

--- 1.Toggle_side_nav/main.js
+++ 1.Toggle_side_nav/main.js
@@ -2,12 +2,7 @@
 const $nav = document.querySelector('.container nav');
 const $toggle = document.querySelector('.toggle');
 let state = false;
-// window.addEventListener('DOMContentLoaded', () => {
-//   localStorage.getItem('toggleStatus') === 'true'
-//     ? $nav.classList.add('active')
-//     : $nav.classList.remove('active');
-// });
-// function ------------------------
+
 const setStatus = newState => {
   localStorage.setItem('toggleStatus', newState);
 };
@@ -17,20 +12,12 @@ const toggleNavigation = state => $nav.classList.toggle('active', state);
 
 // Event bindings -------------------
 window.addEventListener('DOMContentLoaded', () => {
-  // localStorage.clear();
-  getStatus() ? (state = getStatus()) : setStatus(state);
+  getStatus() ? (state = JSON.parse(getStatus())) : setStatus(state);
   toggleNavigation(state);
 });
 
-$toggle.onclick = () => {};
-// $toggle.onclick = () => {
-//   if ($nav.classList.contains('active')) {
-//     localStorage.clear();
-//     localStorage.setItem('toggleStatus', false);
-//     $nav.classList.remove('active');
-//   } else {
-//     localStorage.clear();
-//     localStorage.setItem('toggleStatus', true);
-//     $nav.classList.add('active');
-//   }
-// };
+$toggle.onclick = () => {
+  state = !JSON.parse(getStatus());
+  setStatus(state);
+  toggleNavigation(state);
+};
