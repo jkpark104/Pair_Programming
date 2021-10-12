bc6d5ed30e6305813b652af8ea2c07fbda2534f4
jkpark104 <jkrang104@gmail.com>
2021-10-12 14:37:45 +0900

    feat: Implement Popup

--- 7.Popup/main.js
+++ 7.Popup/main.js
@@ -1,10 +1,46 @@
 // DOM Nodes
 const $togglePopup = document.querySelector('.toggle-popup');
-const $modalContainer = document.querySelector('.modal-container');
 const $modalContainerContainer = document.querySelector(
   '.modal-container-container'
 );
+const $inputMessage = document.querySelector('.input-message');
+const $buttonOk = document.querySelector('.button-ok');
+const $buttonCancel = document.querySelector('.button-cancel');
+const $buttonExit = document.querySelector('.button-exit');
+const $popupMessage = document.querySelector('.popup-message');
+
 // Event bindings
 $togglePopup.onclick = () => {
   $modalContainerContainer.classList.toggle('active');
+  $popupMessage.textContent = '';
+};
+
+$modalContainerContainer.onclick = e => {
+  if (!e.target.classList.contains('modal-container-container')) return;
+  $modalContainerContainer.classList.remove('active');
+};
+
+$buttonOk.onclick = () => {
+  if ($inputMessage.value.trim() !== '') {
+    $popupMessage.textContent = 'from popup : ' + $inputMessage.value.trim();
+  }
+  $inputMessage.value = '';
+  $modalContainerContainer.classList.remove('active');
+};
+
+document.querySelector('.input-message').onkeyup = e => {
+  if (e.key !== 'Enter') return;
+  if ($inputMessage.value.trim() !== '') {
+    $popupMessage.textContent = 'from popup : ' + $inputMessage.value.trim();
+  }
+  $inputMessage.value = '';
+  $modalContainerContainer.classList.remove('active');
+};
+
+$buttonCancel.onclick = () => {
+  $modalContainerContainer.classList.remove('active');
+};
+
+$buttonExit.onclick = () => {
+  $modalContainerContainer.classList.remove('active');
 };
