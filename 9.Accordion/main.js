1015aaae764ff6f8141d457bc2a82f604361f152
kkdd0757 <kkdd0757@hanmail.net>
2021-10-13 09:48:22 +0900

    feat: implement toggleMenu ``

--- 9.Accordion/main.js
+++ 9.Accordion/main.js
@@ -2,6 +2,7 @@
 const $accordion = document.querySelector('.accordion');
 const $menuContainer = document.querySelectorAll('.menu-container');
 const $subMenu = document.querySelectorAll('.submenu');
+const active = [true, false, false];
 
 // $menuContainer[0].onclick = () => {
 // active = !active;
@@ -11,18 +12,27 @@ const $subMenu = document.querySelectorAll('.submenu');
 const toggleMenuContainer = idx => {
   // console.log($subMenu[idx]);
   // $subMenu[idx].classList.toggle('active');
-  console.log($subMenu[idx]);
+  // console.log($subMenu[idx]);
   // if ($subMenu[idx].style.height === '0')
-  //   $subMenu[idx].style.height = `${$subMenu[idx].scrollHeight}px`;
+  // console.log($subMenu[idx].style.height);
+  // else console.log($subMenu[idx].style.height);
+  // $subMenu[idx].style.height = `${$subMenu[idx].scrollHeight}px`;
   // else $subMenu[idx].style.height = '0';
+  // active[idx] = !active[idx];
+  // $subMenu[idx].style.height = active[idx]
+  //   ? `${$subMenu[idx].scrollHeight}px`
+  //   : '0';
+  // [...$menuContainer].forEach(ele => {
+  //   console.log(ele.style);
+  // });
 };
 
 $accordion.onclick = e => {
-  // if ($subMenu[0].style.transition === 'none') {
-  //   console.log('hi');
-  //   $subMenu[0].style.transition = 'height 0.4s ease';
-  // }
   if (!e.target.classList.contains('menu')) return;
+  if ($subMenu[0].style.getPropertyValue('transition-duration')) {
+    $subMenu[0].style.removeProperty('transition-duration');
+  }
+
   const text = e.target.textContent.trim();
   text === 'Front-end'
     ? toggleMenuContainer(0)
@@ -32,7 +42,7 @@ $accordion.onclick = e => {
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-  // $subMenu[0].style.transition = 'none';
-  $subMenu[0].style.setProperty('transition', 'none');
+  // $subMenu[0].style.
+  $subMenu[0].style.setProperty('transition-duration', '0s');
   $subMenu[0].style.height = `${$subMenu[0].scrollHeight}px`;
 });
